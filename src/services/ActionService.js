773cf65e4d7c066a8301e2b26f8ac4ec97777089
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-05-06 19:38:03 -0400

    Build conneconnection with back-end

--- src/services/ActionService.js
+++ src/services/ActionService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_SERVER_POINT;
+const baseURL = process.env.REACT_APP_API_URL;
 
 const service = axios.create({
   baseURL,
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,28 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = process.env.REACT_APP_SERVER_POINT;
+const baseURL = process.env.REACT_APP_API_URL;
 
 const service = axios.create({
   baseURL,
-  withCredentials: true
+  withCredentials: true,
 });
 
 const AUTH_SERVICE = {
   signup(userData) {
-    return service.post('/auth/signup', userData);
+    return service.post("/auth/signup", userData);
   },
 
   login(userData) {
-     return service.post('/auth/login', userData);
+    return service.post("/auth/login", userData);
   },
 
   logout() {
-    return service.post('/auth/logout', {});
+    return service.post("/auth/logout", {});
   },
 
   getUser() {
-    return service.get('/auth/isLoggedIn');
-  }
+    return service.get("/auth/isLoggedIn");
+  },
 };
 
 export default AUTH_SERVICE;
