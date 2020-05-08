8a5112a03ba3cbbdbae2bbbd12d7c9604984a78f
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-05-08 19:18:53 -0400

    Work on UserProfile

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -34,7 +34,7 @@ class UserProfile extends Component {
                     />
                     <Col className="col-8 mt-4 mr-4">
                       <Card className="fixed-height bg-secondary shadow main-container">
-                        <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                        <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4 full-width">
                           <div className="d-flex justify-content-between">
                             <div className="card-profile-image">
                               <a
@@ -49,39 +49,11 @@ class UserProfile extends Component {
                               </a>
                             </div>
                           </div>
-                        </CardHeader>
-                        <CardBody className="pt-0 pt-md-4">
                           <div className="text-center">
-                            <h3>
-                              Jessica Jones
-                              <span className="font-weight-light">, 27</span>
-                            </h3>
-                            <div className="h5 font-weight-300">
-                              <i className="ni location_pin mr-2" />
-                              Bucharest, Romania
-                            </div>
-                            <div className="h5 mt-4">
-                              <i className="ni business_briefcase-24 mr-2" />
-                              Solution Manager - Creative Tim Officer
-                            </div>
-                            <div>
-                              <i className="ni education_hat mr-2" />
-                              University of Computer Science
-                            </div>
-                            <hr className="my-4" />
-                            <p>
-                              Ryan — the name taken by Melbourne-raised,
-                              Brooklyn-based Nick Murphy — writes, performs and
-                              records all of his own music.
-                            </p>
-                            <a
-                              href="#pablo"
-                              onClick={(e) => e.preventDefault()}
-                            >
-                              Show more
-                            </a>
+                            <h3>{currentUser.username}</h3>
                           </div>
-                        </CardBody>
+                        </CardHeader>
+                        <CardBody className="pt-0 pt-md-4"></CardBody>
                       </Card>
                     </Col>
                   </Row>
